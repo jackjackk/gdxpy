465d5c71b2f20951e0927dda54aac75aa3c92144
jackjackk <jackjackk@gmail.com>
2017-06-15 10:40:29 +0200

    improve setup metadata for first release

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from distutils.core import setup
 
 setup(name='gdxpy',
-      version='0.1',
+      version='0.1.0',
+      description='Manipulate GAMS GDX files as Pandas data structures for convenient computation and visualization',
+      url='https://github.com/jackjackk/gdxpy',
       author='Giacomo Marangoni',
       author_email='jackjackk@gmail.com',
-      description='Manipulate GAMS GDX files as Pandas data structures for convenient numerical analysis and visualization',
-      url='https://github.com/jackjackk/gdxpy',
+      license='MIT',
+      keywords='gdx gams pandas',
       packages=['gdxpy',],
+      install_requires=['numpy', 'pandas'],
 )
